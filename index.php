8aa7b165f05c8b02174f9375a073ff812ba83e26
LEGENDARY1209 <130669909+LEGENDARY1209@users.noreply.github.com>
2023-09-29 09:51:40 -0300

    commit do Raí

--- index.php
+++ index.php
@@ -1,32 +1,51 @@
-<?php
-    // Declare as variáveis
-    $var1 = 10;     // Tipo: inteiro
-    $var2 = 7;      // Tipo: inteiro
-    $var3 = "Olá";  // Tipo: string
+<?php 
+    //Declaração de variáveis
+    $valor1 = 27.5;
+    $valor2 = 74;
+    $valor3 = "Fabricio";
 
-    // Verifica se a variável é um número e realiza a multiplicação por 2 se for
-    if (is_numeric($var1)) {
-        $novoVar1 = $var1 * 2;
+    //--------------------------------Valor 1-----------------------------
+    if (is_numeric($valor1)) {
+        $resultado1 = $valor1 * 2;
 
-        // Verifica se o novo número é maior que 100 e imprime a mensagem correspondente
-        if ($novoVar1 > 100) {
-            echo "O novo número (" . $novoVar1 . ") é maior que 100. <br>";
+        if ($resultado1 > 100) {
+            echo "O número " .$resultado1. " é maior que 100";
+        } else {
+            echo "O número " .$resultado1. " não é maior que 100";
         }
+
+    } else {
+        echo $valor1. " não é um número";
     }
+    echo "<br>";
 
-    if (is_numeric($var2)) {
-        $novoVar2 = $var2 * 2;
+    //-------------------------------Valor 2-------------------------------------
+    if (is_numeric($valor2)) {
+        $resultado2 = $valor2 * 2;
 
-        if ($novoVar2 > 100) {
-            echo "O novo número (" . $novoVar2 . ") é maior que 100. <br>";
+        if ($resultado2 > 100) {
+            echo "O número " .$resultado2. " é maior que 100";
+        } else {
+            echo "O número " .$resultado2. " não é maior que 100";
         }
+
+    } else {
+        echo $valor2. " não é um número";
     }
+    echo "<br>";
+    
+    //---------------------------------Valor 3-----------------------------------
+    if (is_numeric($valor3)) {
+        $resultado3 = $valor3 * 2;
 
-    if (is_numeric($var3)) {
-        $novoVar3 = $var3 * 2;
-
-        if ($novoVar3 > 100) {
-            echo "O novo número (" . $novoVar3 . ") é maior que 100. <br>";
+        if ($resultado3 > 100) {
+            echo "O número " .$resultado3. " é maior que 100";
+        } else {
+            echo "O número " .$resultado3. " não é maior que 100";
         }
+
+    } else {
+        echo $valor3. " não é um número";
     }
+    
 ?>
