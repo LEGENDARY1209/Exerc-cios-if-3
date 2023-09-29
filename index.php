ea2ee0c22951c03afbeeab04d7dded66dfdb58bb
LEGENDARY1209 <130669909+LEGENDARY1209@users.noreply.github.com>
2023-09-29 10:10:58 -0300

    commit

--- index.php
+++ index.php
@@ -6,46 +6,28 @@
 
     //--------------------------------Valor 1-----------------------------
     if (is_numeric($valor1)) {
-        $resultado1 = $valor1 * 2;
+        $resultado = $valor1 * 2;
 
-        if ($resultado1 > 100) {
-            echo "O número " .$resultado1. " é maior que 100";
-        } else {
-            echo "O número " .$resultado1. " não é maior que 100";
+        if ($resultado > 100) {
+            echo "O número " . $resultado . " é maior que 100";
         }
-
-    } else {
-        echo $valor1. " não é um número";
     }
-    echo "<br>";
 
     //-------------------------------Valor 2-------------------------------------
     if (is_numeric($valor2)) {
-        $resultado2 = $valor2 * 2;
+        $resultado = $valor2 * 2;
 
-        if ($resultado2 > 100) {
-            echo "O número " .$resultado2. " é maior que 100";
-        } else {
-            echo "O número " .$resultado2. " não é maior que 100";
+        if ($resultado > 100) {
+            echo "O número " . $resultado . " é maior que 100";
         }
-
-    } else {
-        echo $valor2. " não é um número";
     }
-    echo "<br>";
-    
+
     //---------------------------------Valor 3-----------------------------------
     if (is_numeric($valor3)) {
-        $resultado3 = $valor3 * 2;
+        $resultado = $valor3 * 2;
 
-        if ($resultado3 > 100) {
-            echo "O número " .$resultado3. " é maior que 100";
-        } else {
-            echo "O número " .$resultado3. " não é maior que 100";
+        if ($resultado > 100) {
+            echo "O número " . $resultado . " é maior que 100";
         }
-
-    } else {
-        echo $valor3. " não é um número";
     }
-    
 ?>
